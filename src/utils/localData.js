c3753bbd655e66ab20d1576669be0dc589f12d6e
MushlihNur <mushlih.nuris@gmail.com>
2025-01-03 14:10:28 +0700

    fix: fixing eslint

--- src/utils/localData.js
+++ src/utils/localData.js
@@ -1,7 +1,6 @@
 import { FaGithub, FaHtml5, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaBootstrap, FaCss3, FaLaravel, FaReact } from "react-icons/fa6";
 import { SiGmail, SiJavascript, SiMysql, SiPhp, SiTailwindcss } from "react-icons/si";
-import projectErrorImage from '../assets/images/projects/project-error.png';
 
 const skillsData = [
   {
