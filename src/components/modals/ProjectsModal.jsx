fc48f54c7c8e50beeb162375f956645a856d43c3
MushlihNur <mushlih.nuris@gmail.com>
2025-01-03 14:07:05 +0700

    feat (projects): add projects illustration

--- src/components/modals/ProjectsModal.jsx
+++ src/components/modals/ProjectsModal.jsx
@@ -9,7 +9,7 @@ function ProjectsModal() {
         {[...projectsData].reverse().map((project, index) => (
             <motion.div
               key={project?.id}
-              className="bg-[#FFFFFF] flex flex-col gap-4 p-2 rounded-2xl w-full md:w-[280px] lg:w-[280px] xl:w-[300px] shadow-[0_0_16px_rgba(0,0,0,0.15)]"
+              className="bg-[#FFFFFF] flex flex-col gap-4 p-2 rounded-2xl w-full md:w-[280px] lg:w-[280px] xl:w-[300px] shadow-[0_0_16px_rgba(0,0,0,0.15)] group"
               initial={{ rotateY: 90, opacity: 0 }}
               whileInView={{ rotateY: 0, opacity: 1 }}
               viewport={{ once: false }}
@@ -20,7 +20,9 @@ function ProjectsModal() {
               }}
               style={{ transformStyle: "preserve-3d" }}
             >
-              <img src={project?.thumbnail} alt="" className="w-full aspect-video object-cover rounded-lg border-2 border-color-3" />
+              <div className="relative overflow-hidden rounded-lg border-2 border-color-3">
+                <img src={project?.thumbnail} alt="" className="w-full aspect-video object-cover transition-transform duration-300 ease-in-out group-hover:scale-110" />
+              </div>
               <div className="flex flex-col gap-2 text-start">
                 <h2 className="text-xl sm:text-xl xl:text-2xl">{project?.title}</h2>
                 <p className="text-base xl:text-lg">{project?.description}</p>
